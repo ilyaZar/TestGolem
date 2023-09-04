bcd0d24b52dafa1c33fc19b53789abf32e80059e
IlyaZar <zarubin@wiso.uni-koeln.de>
2023-09-04 19:38:25 +0200

    feat: replace data with global-reactive values

--- R/app_server.R
+++ R/app_server.R
@@ -6,6 +6,9 @@
 #' @noRd
 app_server <- function(input, output, session) {
   # Your application server logic
-  data <- shiny::reactiveVal({mtcars})
-  mod_outer_module_server("outer_module")
+  # data <- shiny::reactiveVal({mtcars})
+  global <- reactiveValues(
+    Data = NULL
+  )
+  mod_outer_module_server("outer_module", global = global)
 }
